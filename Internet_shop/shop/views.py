0181938d4f27b5204d2fa16f06d4219ac67016fe
rashidProgram <rashidveluahnov@gmail.com>
2018-04-23 19:18:26 +0300

    dz

--- Internet_shop/shop/views.py
+++ Internet_shop/shop/views.py
@@ -6,9 +6,11 @@ from .models import Shop
 class TovarsListView(ListView):
     template_name = 'shop/list.html'
     model = Shop
+    context_object_name = 'tovars'
 
 
 class TovarsDetailView(DetailView):
     template_name = 'shop/detail.html'
     model = Shop
+    context_object_name = 'tovar'
     queryset = Shop.objects.all()
