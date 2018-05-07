d1395a54ed528fa228e1b44ef254a8589f0443e7
user4 <user4@mail.ru>
2018-05-07 18:01:19 +0300

    m

--- Internet_shop/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, secret
-
-app_name = 'main'
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('secret/', secret, name='secret'),
-]
--- Internet_shop/user/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm, UsernameField
-from django.contrib.auth.models import User
-
-
-class MyUserCreationForm(UserCreationForm):
-    class Meta:
-        model = User
-        field_classes = {'username': UsernameField}
-        fields = ("username", "first_name", "last_name", "email")
